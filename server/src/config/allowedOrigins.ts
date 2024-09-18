c5861b26835af92cf5eda2c8d9846f94912ce7c2
jahidul <jahidulislamapu7620@gmail.com>
2024-09-18 13:13:35 +0100

    cors issue resovled

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -2,6 +2,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'https://jahid-tour-management.vercel.app',
   'https://jahid-bus-ticket-management.vercel.app',
+  'https://jahid-e-ticket-management.vercel.app',
 ]
 
 export default allowedOrigins
